8ce78ef36184c82d01574c2d88da1e0dea15bc55
eric-rafael <ericrafael753@gmail.com>
2021-02-15 10:49:08 -0300

    feitos ajustes

--- calculadora_soma_ifelse.py
+++ calculadora_soma_ifelse.py
@@ -1,13 +1,15 @@
-y= float(input("insira y: "))
-x= float(input("insira x: "))
+print("\nSOMA!\n")
+y= float(input("insira Numero: "))
+x= float(input("insira Numero: "))
 yx= y+x
-print ("soma {} + {} = {}".format( x, y, yx))
+
+print ("\nSoma {} + {} = {}\n".format( x, y, yx))
 
 if yx > 10:
     b = yx
     b += 10
-    print("\ncomo é mais que dez \ncolocarei mais 10, logo,\n{} + 10.0 = {} \n".format(yx, b))
+    print("Como eh mais que dez \ncolocarei mais 10, logo,\n\n{} + 10.0 = {} \n".format(yx, b))
 elif yx == 10:
-    print("igual a dez")
+    print("Igual a Dez\n")
 else:
-    print("menos que dez")
+    print("Menos que Dez\n")
